1c5a1b2fbb96a167a9052d3b4a7614b7375908f9
Ducanh <ducanhdhtb@gmail.com>
2023-08-08 01:41:56 +0700

    exception_edit_02

---
+++ Chapter/Exception/02.py
@@ -0,0 +1,18 @@
+import re
+
+
+def validate_email(email):
+    """ Test if an email is of a valid format """
+
+    if type(email) is not str:
+        raise Exception('Email is not a String')
+
+    regex = '^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$'
+    if not re.search(regex, email):
+        raise Exception('Email is Malformed')
+
+    return True
+
+#validate_email(1)
+#validate_email("hello@udacity")
+validate_email("hello@udacity.com")
