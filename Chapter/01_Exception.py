fc4a8aa5705fa2ce40f72074d42cf8b6f46dd448
Ducanh <ducanhdhtb@gmail.com>
2023-08-08 01:31:20 +0700

    01_Exception.py

---
+++ Chapter/01_Exception.py
@@ -0,0 +1,17 @@
+#print(x)
+
+def divide(a, b):
+    if b == 0:
+        raise Exception("Cannot Divide by Zero")
+    return a/b
+
+def input_divide():
+    try:
+        a = int(input('Divisor: '))
+        b = int(input('Dividend: '))
+        print(f'{a}/{b} = {divide(a, b)}')
+    except Exception as e:
+        print(e)
+        input_divide()
+
+input_divide()
